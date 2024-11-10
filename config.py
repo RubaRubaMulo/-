7d379a7da35774fa46170c849610e94338b936d8
RubaRubaMulo <108836938+RubaRubaMulo@users.noreply.github.com>
2024-11-10 15:17:14 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+token = 'xx:xxxx' # Токен бота
+loginbot = 'xxxx_bot' # Логин бота телеграм
+
+QIWI_PUBLICKEY = 'xxxxxx' #p2p public key
+QIWI_TOKEN = 'xxxxxxxx' # токен 
+QIWI_NUMBER = '77777777777' # Номер карти без +
+
+admin_id = 1111111111 # Ваш айди
+
+price = 190 # Цена за доступ
+
